cb14a34a48d526c8f7ba7fcaf20af77ae1efa141
Austin Howard <ahoward2@unca.edu>
2022-06-09 20:51:51 -0400

    custom cache for details route

--- src/server/api/details/routes/details.ts
+++ src/server/api/details/routes/details.ts
@@ -1,5 +1,38 @@
 import axios from "axios";
 import { Request, Response } from "express";
+import { Logger } from "@nestjs/common";
+
+const logger = new Logger("DETAIL_ROUTE");
+
+const cache = {
+  ttl: 86400, // seconds
+  startTime: Date.now(),
+  dataSetOnce: false,
+  data: {
+    key: null,
+    value: {},
+  },
+};
+
+const useCache = async (req: Request, res: Response) => {
+  if (cache.data.key === null) {
+    cache.data.key = req.path;
+  }
+  const timePassed = Date.now() - cache.startTime;
+  if (
+    Math.floor(timePassed / 1000) >= cache.ttl ||
+    cache.dataSetOnce === false
+  ) {
+    logger.log("Set value to cache");
+    const data = await handler(req, res);
+    cache.data.value = data;
+    cache.startTime = Date.now();
+    cache.dataSetOnce = true;
+  } else {
+    logger.log("Value pulled from cache: ");
+  }
+  res.json(cache.data.value);
+};
 
 /**
  * Aggregates fetches from multiple API endpoints about
@@ -7,28 +40,32 @@ import { Request, Response } from "express";
  */
 async function handler(req: Request, res: Response) {
   if (req.method === "GET") {
-    res.set("Cache-Control", "public, max-age=86400");
     // Make Concurrent API calls
-    axios
-      .all([
-        axios.get(`https://api.github.com/users/ahoward2`),
-        axios.get(`https://gitlab.com/api/v4/users?username=ahoward21`),
-      ])
-      .then(
-        axios.spread((githubData, gitlabData) => {
-          res.json({
-            github: githubData.data,
-            gitlab: gitlabData.data,
-          });
-        })
-      )
+    let data = {};
+
+    async function getGithubData() {
+      return axios.get(`https://api.github.com/users/ahoward2`);
+    }
+
+    async function getGitlabData() {
+      return axios.get(`https://gitlab.com/api/v4/users?username=ahoward21`);
+    }
+
+    await Promise.all([getGithubData(), getGithubData()])
+      .then((result) => {
+        data = {
+          github: result[0].data,
+          gitlab: result[1].data,
+        };
+      })
       .catch((error) => {
         console.error(error);
       });
+    return data;
   }
 }
 
 export const RouteConfig = {
   path: "/details",
-  handler,
+  handler: useCache,
 };
